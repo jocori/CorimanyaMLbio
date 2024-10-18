231d00040f8906d7235885680cfef404e451b3fa
jocori <jocori@ku.edu>
2024-10-18 18:18:51 -0500

    completed step 3 of the assignment: documenting modularity

--- Unit/Unit_modularity/main.R
+++ Unit/Unit_modularity/main.R
@@ -25,14 +25,26 @@ plot(data~year, data = temp)
 colnames(precip)[5]<-"precip"
 colnames(temp)[5]<-"temp"
 
+# MODULARITY EXAMPLE - "4. Define your modules and interfaces precisely in advance." All the numbered blocked code above each funciton was written before any actual code. 
+# This is also true for the inputs and outputs. 
+#So, I knew exactly what needed to go in and come out of each function before ever coding it up. 
+# It also helped me to figure out the best way to split the modules into separate pieces.
+
 ## 1. Filter locations to only ones with at least 40 measurements
 ### input: data: dataset with precipitation and temperature data, var_name (variable name as string ("temp" or "precip")),
 ### min_count (minimum number of measurements)
 ### output: dataset with same column numbers and names as temp or precip that only has rows where each location has at least min_count measurements
-filter_locations <- function(data, var_name, min_count = 40) {
+filter_locations <- function(data, var_name, min_count = 40) { #MODULARITY EXAMPLE- 1. "Do not type constants into your code, give them variable names."
+                                                              # instead of hard-coding the value 40 into the code, I define the minimum count of observations 
+                                                              #  as a named variable so that it is more easily changeable.
   #filter out rows with NA values in the variable
   data %>%
     filter(!is.na(!!sym(var_name))) %>%
+    
+    # MODULARITY EXAMPLE - "7. Write pseudocode." 
+    # Each of these commented lines within the function are leftover pseudocode from 
+    # before I attempted actually coding the function.
+    
     #group data by location name to count measurements per location
     group_by(name) %>%
     #keep those locations that have at least min_count measurements
@@ -60,29 +72,47 @@ get_slopes <- function(data, var_name) {
 ### input: slopes: slope dataframe from the get_slopes function, 
 ###        variable name ("temp" or "precip")
 ### output: none. output is the side effect of the function (the histograms)
-plot_histograms <- function(slopes, var_name){
+
+# MODULARITY EXAMPLE - "6. Copied code is a sign that modularity could help." 
+#I noticed much of the code, input, and output were the same for the functions 
+#that generated the histograms and maps. So, I combined these into a single function.
+plot_mh <- function(slopes, var_name){
+  
+  # MODULARITY EXAMPLE - "8. Be aware of dependencies in R chunks." 
+  # The plot_mh function relies on "slopes" which is an output from the get_slopes function defined above.
+  
   #plot histogram of slope for each variable
     hist(slopes$slope, main = paste("Slope Histogram for", var_name),
          xlab = "Slope", ylab = "Frequency", col = "pink", border = "black")
+  #plot map of region of interest (USA here)
+  map("state")
+  #plot location points, color coded by slope direction
+  points(slopes$lon,slopes$lat, col = ifelse(slopes$slope >0, "lightgreen","coral"), pch = 16,cex = 0.5)
+  #add a title
+  title(main=paste("Slope maps showing", var_name))
 }
 
 ## 4. Make a map for each climate variable where the slopes are depicted in color
 ###   input: slope dataframe from the get_slopes function, 
 ###        variable name ("temp" or "precip")
 ###   output: None. Output is a "side effect" of the funciton (color-coded slopes)
-make_map<- function(slopes,var_name){
+#make_map<- function(slopes,var_name){
     #plot map of region of interest (USA here)
-    map("state")
+   # map("state")
     #plot location points, color coded by slope direction
-    points(slopes$lon,slopes$lat, col = ifelse(slopes$slope >0, "lightgreen","coral"), pch = 16,cex = 0.5)
+  #  points(slopes$lon,slopes$lat, col = ifelse(slopes$slope >0, "lightgreen","coral"), pch = 16,cex = 0.5)
     #add a title
-    title(main=paste("Slope maps showing", var_name))
-}
+  #  title(main=paste("Slope maps showing", var_name))
+#}
 
 ###5. Using the new functions!
 #### input: data_list (list of datasets), variables (vector of variable names)
 # output: Histograms and maps for each variable's slopes
 analysis <- function(data, var_name) {
+  # MODULARITY EXAMPLE - 2. "Write extensible code". The analysis function exemplifies this lesson, 
+  # as the function is built to be able to handle diverse data sets and variables, 
+  #not just those from this assignment. One could pass through a completely new dataset (as long as it had the correct format) 
+  # without the need for modification to the analysis function itself. 
   
   #filter locations for each dataset and variable
     #filter the dataset for the current variable and store it in the list
@@ -92,12 +122,16 @@ analysis <- function(data, var_name) {
   slopes <- get_slopes(filtered_data, var_name)
   
   #plot histograms for each variable
-  plot_histograms(slopes,var_name)
+  plot_mh(slopes,var_name)
   
   #plot maps for each variable
-  make_map(slopes,var_name)
+  #make_map(slopes,var_name)
 }
 
+# MODULARITY EXAMPLE - "3. Rewriting/revising your code can be good and important." 
+# The first version I wrote of this code looped through temperature and precipitation, 
+# eliminating the need to call the datasets separately. 
+# However, the code was much more convoluted, and I revised it to the streamlines approach you now see.
 
 #run the analysis on precipitation data
 analysis(precip, "precip")

bd7eaf615db7a56f99f5a7c59f6f8e756aa8e4fa
Dan Reuman <reuman@ku.edu>
2024-12-22 08:57:47 -0600

    grade, comments, NN

--- Unit/UnitNN/code/main.py
+++ Unit/UnitNN/code/main.py
@@ -1,3 +1,16 @@
+#DAN:
+#I am glad you did something for this assignment, but the dataset is not really one for which
+#sustained investigation with NNs is appropriate. The mushroom data are more suited for CARTs
+#or some basic method. This is reflected in some of your results, like the fact that some of
+#your models are massively overfitted. 
+#
+#One thing that is impressive is you undertook to do everything in python.
+#
+#There are other shortcomings besides what I've listed above, but the above shortcoming
+#kinda prefigures all the others. 
+#
+#Grade: S-
+
 print("Starting Neural Network Assignment Analysis...")
 
 # Run Day 1 and 2
